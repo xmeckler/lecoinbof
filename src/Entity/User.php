d8a0b0d792a80282ebd1fac31ac5a02036c29cbf
Xavier Meckler <xmeckler@gmail.com>
2018-07-13 15:01:50 +0200

    mise en forme des formulaires d'inscription et de connexion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,7 +36,7 @@ class User implements UserInterface, \Serializable
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      */
     private $password;
