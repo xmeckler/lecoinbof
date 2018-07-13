f9e7c7858b00b3d05bc3b455cefccc18e2712f4c
Xavier Meckler <xmeckler@gmail.com>
2018-07-13 11:51:36 +0200

    mise en forme du formulaire de connexion

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,7 +12,7 @@ class HomepageController extends Controller
      */
     public function index()
     {
-        return $this->render('homepage/login.html.twig', [
+        return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
     }
