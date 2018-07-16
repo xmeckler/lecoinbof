06a8557d9cbaea0fc6ce8c7f6ec83f2f9c03ab03
Xavier Meckler <xmeckler@gmail.com>
2018-07-16 13:28:27 +0200

    Mise en page du form de publication d'une annonce

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -38,6 +38,11 @@ class Category
         return $this->id;
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
