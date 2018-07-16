08665f192fcb6f020eca0f13fedff9675acda8ee
Xavier Meckler <xmeckler@gmail.com>
2018-07-16 11:37:05 +0200

    Génération du CRUD pour l'entité Advertisement

---
+++ src/Form/AdvertisementType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Advertisement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdvertisementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('category')
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('image')
+            ->add('city')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Advertisement::class,
+        ]);
+    }
+}
